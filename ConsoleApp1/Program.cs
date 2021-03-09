570948ade977d8d3772e8986db05cfce6739a812
Андрей Orlov <andrejsytenkov@Andrejs-MacBook-Air.local>
2021-03-09 20:40:39 +0300

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int zp_rus_rub = 174000, zp_denmark_kron = 174000;
+        if (zp_rus_rub <= zp_denmark_kron)
+        {
+            Console.WriteLine("Ура, все едем в Д~А~Н~И~Ю на заработки");
+        }
+        else-if(zp_rus_rub >= zp_denmark_kron)
+        {
+            Console.WriteLine("Ура, все едем в РФ на заработки");
+
+        }
+        else
+        {
+            Console.WriteLine("КУДА ЕХАТЬ?");
+        }
+
+    }
+}
