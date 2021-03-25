28f332f3f0db6f8456d0fed9cbb6f12a20c8e94f
Andrei19941967 <sytenkovat@yandex.ru>
2021-03-25 18:10:33 +0300

    task 2

---
+++ 06.2/6work2/6work2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _6work2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double p;
+            int s;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите сумму первоначального взноса в рублях: ");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    if (a < 0)
+                    {
+                        Console.WriteLine("Введено неверное значение. Попробуйте еще раз: ");
+                        continue;
+                    }
+                    Console.WriteLine("Введите ежедневный процент: ");
+                    p = Convert.ToDouble(Console.ReadLine());
+                    if (p < 0)
+                    {
+                        Console.WriteLine("Введено неверное значение. Попробуйте еще раз: ");
+                        continue;
+                    }
+                    Console.WriteLine("Введите желаемую сумму: ");
+                    s = Convert.ToInt32(Console.ReadLine());
+                    if (s < a)
+                    {
+                        Console.WriteLine("Введено неверное значение. Попробуйте еще раз: ");
+                        continue;
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e.Message + ". " + "Попробуйте еще раз: ");
+                    continue;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message + ". " + "Попробуйте еще раз: ");
+                    continue;
+                }
+                break;
+            }
+            int count = 0;
+            while (a < s)
+            {
+                a += (a * p);
+                count++;
+            }
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
