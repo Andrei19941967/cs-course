d85796900ca619c81701a949bb5b231a0b6285b5
Andrei19941967 <sytenkovat@yandex.ru>
2021-03-25 17:46:46 +0300

    task 6

---
+++ 06.1/6work/6work/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _6work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число не более 2 миллиардов: ");
+            int a;
+            while (true)
+            {
+                try
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    if (a < 0 || a > 200000000)
+                    {
+                        Console.WriteLine("Введено неверное значение. Попробуйте еще раз: ");
+                        continue;
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e.Message + ". " + "Попробуйте еще раз: ");
+                    continue;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message + ". " + "Попробуйте еще раз: ");
+                    continue;
+                }
+                break;
+            }
+
+            int count = 0;
+            while (a != 0)
+            {
+                int last = a % 10;
+                if (last % 2 == 0) count++;
+
+                a /= 10;
+            }
+
+            Console.WriteLine("В веденном числе " + count + " четных чисел ");
+
+        }
+    }
+}
