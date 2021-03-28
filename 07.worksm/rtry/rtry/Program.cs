c507f9eb52bbdeeea3f03edd2ed4b20b17b3910d
Andrei19941967 <sytenkovat@yandex.ru>
2021-03-28 15:45:39 +0300

    work

---
+++ 07.worksm/rtry/rtry/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace rtry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two real numbers: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            double n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(n1 + " * " + n2 + " = " + (n1 * n2));
+            Console.WriteLine(String.Format("{0} + {1} = {2}", n1, n2, n2 + n1));
+            Console.WriteLine($"{n1} - {n2} = {n1 - n2}");
+        }
+    }
+}
