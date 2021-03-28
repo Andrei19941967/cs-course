508cb4a1e243c9920980eaed5f209821fd5e22a8
Andrei19941967 <sytenkovat@yandex.ru>
2021-03-28 15:38:16 +0300

    1

--- 07.1/qazw/qazw/Program.cs
+++ 07.1/qazw/qazw/Program.cs
@@ -4,9 +4,49 @@ namespace qazw
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Input string: ");
+
+                string s = "";
+                while (true)
+                {
+                    try
+                    {
+                        s = Console.ReadLine();
+                        if (s.Split().Length < 2)
+                        {
+                            Console.WriteLine("Too few words. Input again: ");
+                            continue;
+                        }
+                    }
+                    catch (OutOfMemoryException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine("Input again: ");
+                        continue;
+                    }
+                    catch (ArgumentOutOfRangeException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine("Input again: ");
+                        continue;
+                    }
+                    break;
+                }
+
+                string[] s_split = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int count = 0;
+                for (int i = 0; i < s_split.Length; i++)
+                {
+                    if (s_split[i][0] == 'A' || s_split[i][0] == 'a' || s_split[i][0] == 'А' || s_split[i][0] == 'а')
+                    {
+                        count++;
+                    }
+                }
+
+                Console.WriteLine(count);
+            }
         }
     }
-}
