90a95b220c141f05386046d53e7b4bc861800ab8
Andrei19941967 <sytenkovat@yandex.ru>
2021-04-04 00:21:20 +0300

    08

---
+++ 08/plm/plm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace plm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 08/plm12/plm12/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace plm12
+{
+    
+    class Program
+    {
+ 
+        public static void Main()
+        {
+            string s= Console.ReadLine();
+
+            Stack<char> st = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '[')
+                {
+                    st.Push(s[i]);
+                }
+                else if (st.Count != 0)
+                {
+                    if (s[i] == ')' && st.Peek() == '(')
+                    {
+                        st.Pop();
+                    }
+                    else if (s[i] == ']' && st.Peek() == '[')
+                    {
+                        st.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                    return;
+                }
+            }
+            if (st.Count == 0) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+    }
+}
+     
