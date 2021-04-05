b39145e2cda6e9e5588b1fd97b16320df3c2ceb8
Andrei19941967 <sytenkovat@yandex.ru>
2021-04-05 16:54:23 +0300

    09

---
+++ 09/ter/ter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = 0;
+            Stopwatch w = new Stopwatch();
+            w.Start();
+            for( int i =1;i<= Math.Sqrt(n);i++)
+            {
+                if(n % i == 0)
+                {
+                    if (n / i != i) m += 2;
+                    else m++;
+                }
+            }
+            w.Stop();
+            Console.WriteLine(m);
+            Console.WriteLine(w.ElapsedMilliseconds);
+        }
+    }
+}
