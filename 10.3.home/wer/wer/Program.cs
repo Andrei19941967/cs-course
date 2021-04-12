8c9e8ca7819967943c53734768698aeb6348ee39
Andrei19941967 <sytenkovat@yandex.ru>
2021-04-12 09:06:54 +0300

    tye

---
+++ 10.3.home/wer/wer/Program.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+
+namespace andrej_OOP
+{
+    class ReminderItem
+    {
+        private DateTimeOffset alarmDate;
+        private string alarmMessage;
+
+
+        public ReminderItem(DateTimeOffset dateTime, string message)
+        {
+            alarmDate = dateTime;
+            alarmMessage = message;
+        }
+
+
+        public DateTimeOffset AlarmDate
+        {
+            get
+            {
+                return alarmDate;
+            }
+
+            set
+            {
+                alarmDate = value;
+            }
+        }
+
+        public string AlarmMessage
+        {
+            get
+            {
+                return alarmMessage;
+            }
+
+            set
+            {
+                alarmMessage = value;
+            }
+        }
+
+        public TimeSpan TimeToAlarm
+        {
+            get
+            {
+                return alarmDate - DateTimeOffset.Now;
+            }
+        }
+
+        public bool IsOutdated
+        {
+            get
+            {
+                if (TimeToAlarm.Milliseconds < 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public void WriteProperties()
+        {
+            Console.WriteLine("Alarm date: " + alarmDate.ToString());
+            Console.WriteLine("Alarm message: " + alarmMessage);
+            Console.WriteLine("Time to alarm: " + TimeToAlarm.ToString());
+            Console.WriteLine("Is out dated: " + IsOutdated);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTimeOffset d1 = new DateTimeOffset(2032, 1, 20, 22, 50, 30, TimeSpan.FromHours(0));
+            string m1 = "Wake up!";
+
+            ReminderItem r1 = new ReminderItem(d1, m1);
+            r1.WriteProperties();
+
+            Console.WriteLine();
+
+            DateTimeOffset d2 = new DateTimeOffset(2000, 1, 20, 22, 50, 30, TimeSpan.FromHours(0));
+            string m2 = "Wake up, dear!";
+
+            ReminderItem r2 = new ReminderItem(d2, m2);
+            r2.WriteProperties();
+        }
+    }
+}
